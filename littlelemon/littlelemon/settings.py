913ecca4bd5065100c548be062217d57bee5a38a
duanjs7 <duancollege@outlook.com>
2023-07-27 15:56:30 +0800

    add statics and configs

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'littlelemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['restaurant/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,8 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'restaurant/static/'
 
+STATICFILES_DIRS = [
+    "restaurant/static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
